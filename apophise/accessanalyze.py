b96cde602f68b2a1e0408f6462c6f5f613918f05
apophise <zbzcsn@qq.com>
2015-07-21 09:06:38 +0800

    newOM

---
+++ apophise/accessanalyze.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding=utf-8
+# ************************************************************************/
+#	> Author: cooperzhang
+#	> Mail: zbzcsn@qq.com
+#	> Created Time: Thu 09 Jul 2015 08:12:29 PM CST
+# ************************************************************************/
+
+#todo: to analyze the access log
