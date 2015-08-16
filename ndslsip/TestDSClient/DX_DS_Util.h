41d53cbb24581cadc775006218032321af491eac
apophise <zbzcsn@qq.com>
2015-08-16 14:20:37 +0000

    test_dian xin gb28181

---
+++ ndslsip/TestDSClient/DX_DS_Util.h
@@ -0,0 +1,19 @@
+/*************************************************************************
+	> File Name: DX_DS_Util.h
+	> Author: cooperz
+	> Mail: zbzcsn@qq.com
+	> Created Time: Sun 16 Aug 2015 07:50:30 AM UTC
+ ************************************************************************/
+
+#ifndef _DX_DS_UTIL_H
+#define _DX_DS_UTIL_H
+
+#define DX_SIP_DEV_NAME ("34010000002000000001")
+#define DX_SIP_PASSWD ("123456")
+#define DX_SIP_IP ("192.168.250.1")
+#define DX_DS_DEV_NAME ("34010000004000010221")
+#define DX_DS_IP ("192.168.183.131")
+#define DX_DS_PORT_STR ("5059")
+#define DX_SIP_PORT_STR ("5060")
+
+#endif
